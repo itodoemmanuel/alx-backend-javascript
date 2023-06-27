656b135564a020ef12eec172ce55c3f0ad0b30de
Itodo Emmanuel Itodo <54606115+itodoemmanuel@users.noreply.github.com>
2023-06-27 18:32:38 +0100

    Delete 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++
@@ -1,9 +0,0 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (!success) reject(new Error('The fake API is not working currently'));
-    resolve({
-      status: 200,
-      body: 'Success',
-    });
-  });
-}
